4dc3c79fc33aa303d80a8f45f9159816e856d66f
Amardeep <amardeep.rox@gmail.com>
2025-07-18 15:12:22 +0530

    further enhancements

--- app/tasks/broker.py
+++ app/tasks/broker.py
@@ -3,11 +3,9 @@
 
 import dramatiq
 from dramatiq_pg import PostgresBroker
-from dramatiq.brokers.stub import StubBroker
 from dramatiq.middleware import CurrentMessage
 from periodiq import PeriodiqMiddleware
-from dramatiq.results import Results
-from dramatiq.results.backends.stub import StubBackend
+
 from ..settings import settings
 
 # --- production broker ------------------------------------------------------
@@ -15,10 +13,6 @@ postgres_broker = PostgresBroker(url=settings.database_url)
 postgres_broker.add_middleware(CurrentMessage())
 postgres_broker.add_middleware(PeriodiqMiddleware(skip_delay=30))
 
-# --- in‑memory broker for tests ---------------------------------------------
-stub_broker = StubBroker()
-stub_broker.add_middleware(Results(backend=StubBackend()))
-stub_broker.add_middleware(PeriodiqMiddleware(skip_delay=30))
 
 # --- choose at import time ---------------------------------------------------
-dramatiq.set_broker(stub_broker if settings.testing else postgres_broker)
+dramatiq.set_broker(postgres_broker)
